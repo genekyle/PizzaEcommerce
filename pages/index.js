82b5e0b334f46dfcf5af77024abb3139a62917b6
Gene Kyle Magsipoc <48112543+genekyle@users.noreply.github.com>
2022-01-15 18:40:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export const getServerSideProps = async (ctx) => {
     admin = true
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get("http://pizza-ecommerce-two.vercel.app/api/products");
   return{
     props:{
       productsList:res.data,

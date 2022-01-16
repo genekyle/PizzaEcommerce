9c934dfc4d164710a046c79aa5f3a79795b10003
Gene Kyle Magsipoc <48112543+genekyle@users.noreply.github.com>
2022-01-16 17:11:16 -0500

    Update Add.jsx

--- components/Add.jsx
+++ components/Add.jsx
@@ -44,7 +44,7 @@ function Add({ setClose }) {
                 img: url,
             };
 
-            await axios.post("http://pizza-ecommerce-two.vercel.app/api/products", newProduct);
+            await axios.post("https://pizza-ecommerce-tau.vercel.app/api/products", newProduct);
             setClose(true)
 
         }catch(err) {

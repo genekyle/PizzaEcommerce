5f5f6003acc250c1f4e24748085d7d3ba555b76e
Gene Kyle Magsipoc <48112543+genekyle@users.noreply.github.com>
2022-01-15 19:31:46 -0500

    Update ProductsList.jsx
    typo

--- components/ProductsList.jsx
+++ components/ProductsList.jsx
@@ -9,7 +9,7 @@ function ProductsList( {productsList} ) {
                     BEST PIZZAS IN THE CITY
                 </h1>
                 <p className={styles.desc}>
-                    Try and order one of our pizzas! To acces the admin dashboard by typing &quot;/admin&ldquo; at the end of the url, in the dashboard you will be able to upload your own pizza and see how other orders are going! (user: admin)(pass: 123456)
+                    Try and order one of our pizzas! To access the admin dashboard by typing &quot;/admin&ldquo; at the end of the url, in the dashboard you will be able to upload your own pizza and see how other orders are going! (user: admin)(pass: 123456)
                 </p>
                 <div className={styles.wrapper}>
                     {productsList.map((pizza) => (
